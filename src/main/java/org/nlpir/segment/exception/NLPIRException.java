0624dcac700273c18a01d5f95d8ec02ddb6d1f60
Hongyan Pan <1107724781@qq.com>
2018-03-08 16:33:41 +0800

    Fix bugs

--- src/main/java/org/nlpir/segment/exception/NLPIRException.java
+++ src/main/java/org/nlpir/segment/exception/NLPIRException.java
@@ -1,5 +1,10 @@
 package org.nlpir.segment.exception;
 
+/**
+ * 分词异常类
+ * @author panhongyan
+ *
+ */
 public class NLPIRException extends Exception{
 
 	private static final long serialVersionUID = 6897134322647598254L;
